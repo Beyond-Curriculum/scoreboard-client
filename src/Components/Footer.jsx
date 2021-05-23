5a2462ac504fd73d946a646df9680267ac173006
Anton Morgunov <a.mor@bc-pf.org>
2021-05-23 18:45:10 +0600

    meta and footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -17,10 +17,10 @@ const Footer = ({ lang }) => {
                     }
                 }} className={classes.dark}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg></button>
                 <div className={classes.label}>Created by</div>
-                <a href="https://beyondcurriculum.kz"><img src={beyond} height={32} alt="BeyondCurriculum" /></a>
+                <a href="https://bc-pf.org"><img src={beyond} height={32} alt="BeyondCurriculum" /></a>
                 <div className={classes.flex}>
-                    <div className={classes.copyright}>© <a href="https://beyondcurriculum.kz">BeyondCurriculum</a> 2020</div>
-                    <a className={classes.link} href="https://beyondcurriculum.kz/donate">{l.support + " "}<span aria-label="heart" role="img">❤️</span></a>
+                    <div className={classes.copyright}>© <a href="https://bc-pf.org">Beyond Curriculum PF</a> 2020-2021</div>
+                    <a className={classes.link} href="https://bc-pf.org/donate">{l.support + " "}<span aria-label="heart" role="img">❤️</span></a>
                 </div>
             </div>
         </footer>
